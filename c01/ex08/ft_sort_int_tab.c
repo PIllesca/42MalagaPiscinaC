e00d559865cc9cccbe1e1af9f876bdb9ead35bf2
PIllesca <142722501+PIllesca@users.noreply.github.com>
2023-08-20 12:33:05 +0200

    Add files via upload

---
+++ c01/ex08/ft_sort_int_tab.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pillesca <pillesca@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/14 19:48:15 by pillesca          #+#    #+#             */
+/*   Updated: 2023/08/15 17:09:03 by pillesca         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int	i;
+	int	j;
+	int	tmp;
+
+	i = 0;
+	while (i < size - 1)
+	{
+		j = i + 1;
+		while (j < size)
+		{
+			if (tab[i] > tab[j])
+			{
+				tmp = tab[i];
+				tab[i] = tab[j];
+				tab[j] = tmp;
+			}
+			j++;
+		}
+		i++;
+	}
+}
