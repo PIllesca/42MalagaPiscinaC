6a28ff4faa2ddd63a2d6e6acc3c59307253c9e74
PIllesca <142722501+PIllesca@users.noreply.github.com>
2023-08-20 18:21:56 +0200

    Update test.c

--- rush01/test.c
+++ rush01/test.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+#include <stdio.h>
 #define SIZE 4
 #define UP 0
 #define DW 1
@@ -112,7 +113,7 @@ void init_matrix(int matrix[SIZE][SIZE]){
     }
 }
 // Rellenar tamaño máximo
-void ft_fillMax(int matrix[SIZE][SIZE], int col[2][SIZE], int row[2][SIZE])
+void ft_fill_max(int matrix[SIZE][SIZE], int col[2][SIZE], int row[2][SIZE])
 {
     int i;
     int j;
@@ -153,10 +154,94 @@ void ft_fillMax(int matrix[SIZE][SIZE], int col[2][SIZE], int row[2][SIZE])
         i++;
     }
 }
+void ft_fill_four(int matrix[SIZE][SIZE], int col[2][SIZE], int row[2][SIZE])
+{
+    int i;
+
+    i = 0;
+    while (i < SIZE)
+    {
+        if (col[UP][i] == 2 && matrix[0][i] == 3)
+            matrix[1][i] = 4;
+        if (row[LF][i] == 2 && matrix[i][0] == 3)
+            matrix[i][1] = 4;
+        if (col[DW][i] == 2 && matrix[SIZE - 1][i] == 3)
+            matrix[SIZE - 2][i] = 4;
+        if (row[RG][i] == 2 && matrix[i][SIZE - 1] == 3)
+            matrix[i][SIZE - 2] = 4;
+        i++;
+    }
+}
+void ft_fill_three(int matrix[SIZE][SIZE], int col[2][SIZE], int row[2][SIZE])
+{
+    int i;
+
+    i = 0;
+    while (i < SIZE)
+    {
+        if (col[UP][i] == 3 && matrix[0][i] == 2 && matrix[2][i] == 4)
+            matrix[1][i] = 3;
+        if (row[LF][i] == 3 && matrix[i][0] == 2 && matrix[i][2] == 4)
+            matrix[i][1] = 3;
+        if (col[DW][i] == 3 && matrix[3][i] == 2 && matrix[1][i] == 4)
+            matrix[2][i] = 3;
+        if (row[RG][i] == 3 && matrix[i][3] == 2 && matrix[i][1] == 4)
+            matrix[i][2] = 3;
+        if (col[UP][i] == 2 && matrix[3][i] == 4)
+            matrix[0][i] = 3;
+        if (row[LF][i] == 2 && matrix[i][3] == 4)
+            matrix[i][0] = 3;
+        if (col[DW][i] == 2 && matrix[0][i] == 4)
+            matrix[3][i] = 3;
+        if (row[RG][i] == 2 && matrix[i][0] == 4)
+            matrix[i][3] = 3;
+        i++;
+    }
+}
+// Si la condición es dos y el tercero a la condición es 4 poner 2 en 0
+void ft_fill_two(int matrix[SIZE][SIZE], int col[2][SIZE], int row[2][SIZE])
+{
+    int i;
+
+    i = 0;
+    while (i < SIZE)
+    {
+        if (col[UP][i] == 2 && matrix[2][i]  == 4)
+            matrix[0][i] = 2;
+        if (row[LF][i] == 2 && matrix[i][2] == 4)
+            matrix[i][0] = 2;
+        if (col[DW][i] == 2 && matrix[1][i] == 4)
+            matrix[3][i] = 2;
+        if (row[RG][i] == 2 && matrix[i][1] == 4)
+            matrix[i][3] = 2;
+        i++;
+    }
+}
+// Los restantes son 0
+void ft_fill_rest(int matrix[SIZE][SIZE])
+{
+    int rw = 0;
+    int cl = 0;
+    while (cl < SIZE)
+    {
+        rw = 0;
+        while (rw < SIZE)
+        {
+            if (matrix[cl][rw] == 0)
+                matrix[cl][rw] = 1;
+            rw++;
+        }
+        cl++;
+    }
+}
 // Rellenar la matriz
 void ft_fillmatrix(int matrix[SIZE][SIZE], int col[2][SIZE], int row[2][SIZE])
 {
-    ft_fillMax(matrix, col, row);
+    ft_fill_max(matrix, col, row);
+    ft_fill_four(matrix, col, row);
+    ft_fill_three(matrix, col, row);
+    ft_fill_two(matrix, col, row);
+    ft_fill_rest(matrix);
 }
 int main(void){
     char str[] = "4 3 2 1 1 2 2 2 4 3 2 1 1 2 2 2";
