db4359aa423202e9e9afd9fc2f2bad5846cb58bc
PIllesca <142722501+PIllesca@users.noreply.github.com>
2023-08-24 13:58:54 +0200

    Add files via upload

--- c04/ex04/ft_putnbr_base.c
+++ c04/ex04/ft_putnbr_base.c
@@ -6,7 +6,7 @@
 /*   By: pillesca <pillesca@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/24 12:52:12 by pillesca          #+#    #+#             */
-/*   Updated: 2023/08/24 13:30:59 by pillesca         ###   ########.fr       */
+/*   Updated: 2023/08/24 13:56:11 by pillesca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
@@ -37,6 +37,38 @@ int	ft_chkbase(char *base)
 	return (size);
 }
 
+int	ft_chkneg(int num, char *base, int size)
+{
+	int	i;
+
+	if (num < 0)
+	{
+		write (1, "-", 1);
+		if (num == -2147483648)
+		{
+			i = (num / size);
+			write (1, &base[i * -1], 1);
+			num = i;
+		}
+		num *= -1;
+	}
+	return (num);
+}
+
+void	ft_printnum(int num, char *base, int size)
+{
+	int	i;
+
+	num = ft_chkneg(num, base, size);
+	while (num > size)
+	{
+		i = num / size;
+		write (1, &base[i], 1);
+		num = i;
+	}
+	write (1, &base[num % size], 1);
+}
+
 void	ft_putnbr_base(int nbr, char *base)
 {
 	int	size;
@@ -44,4 +76,5 @@ void	ft_putnbr_base(int nbr, char *base)
 	size = ft_chkbase(base);
 	if (size < 2)
 		return ;
+	ft_printnum(nbr, base, size);
 }
