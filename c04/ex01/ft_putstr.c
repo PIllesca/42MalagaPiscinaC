4d41690c10805c8293a24e3d8c333cbc90831f5d
PIllesca <142722501+PIllesca@users.noreply.github.com>
2023-08-24 20:21:56 +0200

    Add files via upload

--- c04/ex01/ft_putstr.c
+++ c04/ex01/ft_putstr.c
@@ -6,7 +6,7 @@
 /*   By: pillesca <pillesca@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/22 16:52:32 by pillesca          #+#    #+#             */
-/*   Updated: 2023/08/23 12:43:38 by pillesca         ###   ########.fr       */
+/*   Updated: 2023/08/24 19:54:04 by pillesca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
@@ -14,7 +14,7 @@
 void	ft_putstr(char *str)
 {
 	if (str == 0)
-		return (0);
+		return ;
 	while (*str)
 	{
 		write(1, str, 1);
--- c04/ex03/ft_atoi.c
+++ c04/ex03/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: pillesca <pillesca@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/23 12:46:40 by pillesca          #+#    #+#             */
-/*   Updated: 2023/08/24 12:45:50 by pillesca         ###   ########.fr       */
+/*   Updated: 2023/08/24 20:11:00 by pillesca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 int	ft_chksig(char *str, char **rptr)
@@ -59,6 +59,8 @@ int	ft_getstrnum(char *str, int size, int neg)
 
 	rtn = 0;
 	decenas = 1;
+	while (*str && *str == '0')
+		str++;
 	while (size > 0)
 	{
 		rtn += (str[size - 1] - '0') * decenas;
