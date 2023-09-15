33f6883461e42013b0e38ae624941bffbb128ea1
PIllesca <142722501+PIllesca@users.noreply.github.com>
2023-09-15 22:40:06 +0200

    Update ft_strncmp.c

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -17,10 +17,12 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	while (*s1 && *s2 && n > 0)
+	while (n > 0)
 	{
-		if (*s1 != *s2)
-			return (*s1 - *s2);
+		if ((unsigned char)*s1 != (unsigned char)*s2)
+			return ((unsigned char)*s1 - (unsigned char)*s2);
+		if ((unsigned char) !*s1 || (unsigned char) !*s2)
+			return (0);
 		n--;
 		s1++;
 		s2++;
