acd0ff97eed7e9fbbcad3e0909c911941812911e
PIllesca <142722501+PIllesca@users.noreply.github.com>
2023-09-26 12:59:42 +0200

    Add files via upload

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -6,35 +6,37 @@
 /*   By: pillesca <pillesca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/25 13:01:06 by pillesca          #+#    #+#             */
-/*   Updated: 2023/09/25 13:50:27 by pillesca         ###   ########.fr       */
+/*   Updated: 2023/09/26 12:44:55 by pillesca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 // Función que crea una nueva lista resultado de aplicar la función f a el
-// contenido de cada nodo de la lista lst.
+// contenido de cada nodo de la lista lst. data es el contenido de cada nodo,
+// añadido para poder liberar la memoria en caso de error.
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	*old;
 	t_list	*new;
 	t_list	*ptr;
+	void	*data;
 
 	if (!lst || !f || !del)
 		return (NULL);
-	old = lst;
 	new = NULL;
-	while (old)
+	while (lst)
 	{
-		ptr = ft_lstnew(f(old->content));
+		data = f(lst->content);
+		ptr = ft_lstnew(data);
 		if (!ptr)
 		{
 			ft_lstclear(&new, del);
+			free(data);
 			return (NULL);
 		}
 		ft_lstadd_back(&new, ptr);
-		old = old->next;
+		lst = lst->next;
 	}
 	return (new);
 }
