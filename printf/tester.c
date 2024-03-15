0019ac16ccb85c144f1db2385018141f2aa2412c
Pablo Illescas Ruiz <142722501+PIllesca@users.noreply.github.com>
2024-03-15 13:31:55 +0100

    Add files via upload

--- printf/tester.c
+++ printf/tester.c
@@ -6,7 +6,7 @@
 /*   By: pillesca <pillesca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/07 13:32:28 by pillesca          #+#    #+#             */
-/*   Updated: 2024/03/07 13:33:01 by pillesca         ###   ########.fr       */
+/*   Updated: 2024/03/15 13:15:34 by pillesca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 int	main(void)
 {
-	ft_printf("Hola Mundo.");
+	ft_printf("H%cla %%Mundo. %d %i %u \n", 'o', 15, -23, -2147483648);
+	ft_printf("%d %u %u\n", -2147483648, 2147483647, -1);
 	return (0);
 }
